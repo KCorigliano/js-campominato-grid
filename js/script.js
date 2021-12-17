6b40fdb794f69a854faf1f85b084071efa0733da
KCorigliano <corigliano98@hotmail.com>
2021-12-17 15:13:24 +0100

    numerazione box

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function createNewBox(container) {
     const square = document.createElement('div');
     square.className = 'box';
+    square.innerHTML += i
     square.addEventListener ('click', function(){
-        square.classList.toggle('azure');
+        square.classList.add('azure');
+        square.innerHTML = '';
     })
     container.append(square);
 }
@@ -11,6 +13,7 @@ function createNewBox(container) {
 function createContainer(numMax){
     for (i=1; i <= numMax; i++){
         createNewBox(containerElement);
+        
     }
 };
 
@@ -20,25 +23,40 @@ function hideElement(element1, element2, element3){
     element3.classList.add('hidden');
 }
 
+function activeElement(container, element){
+    container.style.justifyContent = "center";
+    element.classList.add('active');
+    element.classList.add('center');
+}
+
 const containerElement = document.querySelector('.container-game');
+const containerButtonElement = document.querySelector('.container-button');
 const button1 = document.getElementById('level-1');
 const button2 = document.getElementById('level-2');
 const button3 = document.getElementById('level-3');
+const refreshButton = document.getElementById('refresh');
 
 button1.addEventListener ('click', function(){
     hideElement(button1, button2, button3);
+    activeElement(containerButtonElement, refreshButton);
     containerElement.classList.toggle('lvl1');
     createContainer(100);
 })
 
 button2.addEventListener ('click', function(){
     hideElement(button1, button2, button3);
+    activeElement(containerButtonElement,refreshButton);
     containerElement.classList.toggle('lvl2');
     createContainer(81);
 })
 
 button3.addEventListener ('click', function(){
     hideElement(button1, button2, button3);
+    activeElement(containerButtonElement, refreshButton);
     containerElement.classList.toggle('lvl3');
     createContainer(49);
+})
+
+refreshButton.addEventListener ('click', function(){
+    location.reload();
 })
