d59ad878fe6de452303ac66aaa6ba1374cca1619
KCorigliano <corigliano98@hotmail.com>
2021-12-20 10:13:11 +0100

    fix function

--- js/script.js
+++ js/script.js
@@ -8,15 +8,14 @@ function createNewBox(container) {
     container.append(square);
 }
 
-function createContainer(numMax){
-    containerElement.innerHTML = '';
+function createContainer(numMax, container){
+    container.innerHTML = '';
     for (i=1; i <= numMax; i++){
-        createNewBox(containerElement);
+        createNewBox(container);
     }
 };
 
 const containerElement = document.querySelector('.container-game');
-const containerButtonElement = document.querySelector('.container-button');
 const button1 = document.getElementById('level-1');
 const button2 = document.getElementById('level-2');
 const button3 = document.getElementById('level-3');
@@ -24,17 +23,17 @@ const button3 = document.getElementById('level-3');
 button1.addEventListener ('click', function(){
     containerElement.classList.remove('lvl1', 'lvl2', 'lvl3')
     containerElement.classList.toggle('lvl1');
-    createContainer(100);
+    createContainer(100, containerElement);
 })
 
 button2.addEventListener ('click', function(){
     containerElement.classList.remove('lvl1', 'lvl2', 'lvl3')
     containerElement.classList.toggle('lvl2');
-    createContainer(81);
+    createContainer(81, containerElement);
 })
 
 button3.addEventListener ('click', function(){
     containerElement.classList.remove('lvl1', 'lvl2', 'lvl3')
     containerElement.classList.toggle('lvl3');
-    createContainer(49);
+    createContainer(49, containerElement);
 })
